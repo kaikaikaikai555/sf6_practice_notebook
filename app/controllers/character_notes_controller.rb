da4e9b42a6bff7a4b1713b35b56ec78903c5f37c
Kaito <sigeru20020310@gmail.com>
2026-08-21 04:03:16 +0900

    feat: 対策メモの一覧・作成・削除機能とルーティングの実装

--- app/controllers/character_notes_controller.rb
+++ app/controllers/character_notes_controller.rb
@@ -1,22 +1,36 @@
 class CharacterNotesController < ApplicationController
   def index
+    @character_notes = CharacterNote.all
   end
 
   def new
+    @character_note = CharacterNote.new
   end
 
   def create
+    @character_note = CharacterNote.new(character_note_params)
+    @character_note.user = User.first_or_create(name: "テストユーザー", email: "test@example.com")
+
+    if @character_note.save
+      redirect_to character_notes_path, notice: "対策メモを保存しました！"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
-  end
-
-  def edit
-  end
-
-  def update
+    @character_note = CharacterNote.find(params[:id])
   end
 
   def destroy
+    @character_note = CharacterNote.find(params[:id])
+    @character_note.destroy
+    redirect_to character_notes_path, notice: "対策メモを削除しました！"
+  end
+
+  private
+
+  def character_note_params
+    params.require(:character_note).permit(:opponent_character, :my_character, :quick_summary, :content)
   end
 end
