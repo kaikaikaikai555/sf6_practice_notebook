591ae9d186bd917a70e22f85f34f07304e6693b5
Kaito <sigeru20020310@gmail.com>
2026-08-24 19:11:02 +0900

    Phase2: 10秒確認機能・検索機能・編集機能を実装

--- app/models/character_note.rb
+++ app/models/character_note.rb
@@ -1,3 +1,4 @@
 class CharacterNote < ApplicationRecord
-  belongs_to :user
+  # 対戦メモが削除されたら、紐づく対戦ログも一緒に削除する設定
+  has_many :match_logs, dependent: :destroy
 end
--- app/models/match_log.rb
+++ app/models/match_log.rb
@@ -1,5 +1,5 @@
 class MatchLog < ApplicationRecord
-  belongs_to :user
+  belongs_to :character_note
   has_many :match_log_defeat_tags, dependent: :destroy
   has_many :defeat_tags, through: :match_log_defeat_tags
 end
